1ccd342671b3e9d4a3d48c3f99cf151f583d8023
fabienchapuis <fabien-chapuis@netc.fr>
2019-06-20 14:43:12 +0000

    tuto sur synfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Twig\Environment;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PropertyRepository;
+use Cocur\Slugify\Slugify;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @param PropertyRepository $repository
+     * @return Response
+     */
+    public function index(PropertyRepository $repository): Response
+    {
+        $properties = $repository->findLatest();
+        return $this->render('pages/home.html.twig',[
+            'properties' => $properties
+        ]);
+    }
+
+
+
+}
